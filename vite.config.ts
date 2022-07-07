ae9de514672e0672649e7e6b7409c97c074fc12b
YernarT <toktaryernar@gmail.com>
2022-07-07 23:48:04 +0600

    init: :rainbow: ts & vite config

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+function pathResolve(dir: string): string {
+	return resolve(process.cwd(), '.', dir);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: pathResolve('src') + '/',
+			},
+		],
+	},
+
+	plugins: [react()],
+});
