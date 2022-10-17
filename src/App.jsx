f8da62bf9bfc8d1db3e9f3fd18fd4dcd993f6f82
YernarT <toktaryernar@gmail.com>
2022-10-17 15:04:36 +0600

    init: :rainbow: 初始化 main, App

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// React
+import { useRef, useEffect } from 'react';
+
+// 组件
+import { Adv } from '@/components';
+// 全局样式
+import '@/assets/style/reset.css';
+
+function App() {
+	return (
+		<>
+			<Adv />
+
+			{/* <AppStyled> */}
+			{/* <Canvas ref={canvasRef} /> */}
+			{/* </AppStyled> */}
+		</>
+	);
+}
+
+export default App;
