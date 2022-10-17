2caa411fc1ea769da9af54ecc1b6c9a608139b3f
YernarT <toktaryernar@gmail.com>
2022-10-17 19:39:46 +0600

    feat: :sparkles: container-padding variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-// React
-import { useRef, useEffect } from 'react';
+import { useMount, useEventListener, useMemoizedFn } from 'ahooks';
 
 // 组件
 import { Adv, Navbar, Header } from '@/components';
@@ -7,6 +6,35 @@ import { Adv, Navbar, Header } from '@/components';
 import '@/assets/style/reset.css';
 
 function App() {
+	// 计算 css 变量 --container-padding
+	const calcConatinerPadding = useMemoizedFn(() => {
+		const { clientWidth } = document.body;
+		const containerMaxWidth = 980;
+		let padding;
+
+		if (clientWidth > containerMaxWidth) {
+			padding = clientWidth - containerMaxWidth;
+		} else {
+			padding = 0;
+		}
+
+		return padding / 2;
+	});
+
+	// 初始化 css 变量 --container-padding
+	useMount(() => {
+		document.body.style.cssText += `
+			--container-padding: ${calcConatinerPadding()}px;
+		`;
+	});
+
+	// 监视/维护 css 变量 --container-padding
+	useEventListener('resize', () => {
+		document.body.style.cssText += `
+			--container-padding: ${calcConatinerPadding()}px;
+		`;
+	});
+
 	return (
 		<>
 			<Adv />
