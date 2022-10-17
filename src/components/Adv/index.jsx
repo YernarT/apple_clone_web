d30e9eed869c606b3f9cace082a0230fbe605236
YernarT <toktaryernar@gmail.com>
2022-10-17 15:54:21 +0600

    feat: :sparkles: Adv component

---
+++ src/components/Adv/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.less';
+
+export default function Adv() {
+	return (
+		<div className={styles.adv}>
+			<h4 className="content">
+				Products, services, and OS functions may not be available in this
+				country.
+			</h4>
+		</div>
+	);
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as Adv } from './Adv';
