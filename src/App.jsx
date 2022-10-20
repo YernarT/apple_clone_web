afda54ced7a57e06681d288c6418095e628fb0cd
YernarT <toktaryernar@gmail.com>
2022-10-20 17:23:38 +0600

    update: :hammer: add MacBook Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useMount, useEventListener, useMemoizedFn } from 'ahooks';
 
 // 组件
-import { Adv, Navbar, Header, Intro } from '@/components';
+import { Adv, Navbar, Header, Intro, MacBook } from '@/components';
 // 全局样式
 import '@/assets/style/reset.css';
 
@@ -41,6 +41,7 @@ function App() {
 			<Navbar />
 			<Header />
 			<Intro />
+			<MacBook />
 		</>
 	);
 }
